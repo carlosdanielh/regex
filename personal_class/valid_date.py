e7d5ac2ee009a31eb4d78f17d0f4089faa608095
silverchair <carlosdanielh@mail.com>
2020-12-12 12:56:17 -0400

    clase terminada, trabajar en clase

--- personal_class/valid_date.py
+++ personal_class/valid_date.py
@@ -17,3 +17,15 @@ class Valid_Date():
     def formatted_latin_date(self):
         x = datetime(self.year, self.month, self.day)
         return x.strftime('%d/%m/%y')
+
+    def formatted_latin_date_list(self, list_):
+        date_formatted_list = []
+        for element in list_:            
+            date = element.split('/')
+            day = int(date[0])
+            month = int(date[1])
+            year = int(date[2])
+            date = Valid_Date(day, month, year)
+            if date.is_valid_date():
+                date_formatted_list.append(date.formatted_latin_date())
+        print(date_formatted_list)
